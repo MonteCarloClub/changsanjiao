f68afa0fa537827108f8db415755e9e9987ee1db
Bruce <45781633+Tao1998@users.noreply.github.com>
2022-09-25 22:24:52 +0800

    第三个场景

--- src/common/item.ts
+++ src/common/item.ts
@@ -1,6 +1,7 @@
 import contract from "@/assets/contract.svg";
 import record from "@/assets/records.svg";
 import credential from "@/assets/verify.svg";
+import address from "@/assets/address.svg";
 
 /**
  * 一个角色
@@ -9,11 +10,12 @@ export type Item = {
   x: number;
   y: number;
   title?: string;
-  type: 'contract' | 'records' | 'credential';
+  type: 'contract' | 'records' | 'credential' | 'address';
 };
 
 export const itemIcons = {
     contract,
     record,
-    credential
+    credential,
+    address
 }
